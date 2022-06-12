2e15ba52c60735c0cbd118ba07e00c6d9789091e
root <root@dellpc>
2022-06-12 21:44:43 +0100

    disabled promiscuous

--- log-last-arp.sh
+++ log-last-arp.sh
@@ -2,5 +2,5 @@
 #log the unix timestamp of current time
 while :
 do 
-	tcpdump 'arp and dst 192.168.0.102' -l | while read line ; do date '+%s' >> /var/log/log-last-arp.log; done ;
+	tcpdump 'arp and dst 192.168.0.102 and src not 192.168.0.1' -l -p | while read line ; do date '+%s' >> /var/log/log-last-arp.log; done ;
 done
